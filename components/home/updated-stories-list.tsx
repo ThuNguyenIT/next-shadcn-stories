62603c03ab07aa8ae6fadbf0f56de3cd10da9f61
haxuanvu870 <haxuanvu870@gmail.com>
2024-10-07 22:51:22 +0700

    up

--- components/home/updated-stories-list.tsx
+++ components/home/updated-stories-list.tsx
@@ -11,7 +11,7 @@ export const UpdatedStoriesList: React.FC<IUpdatedStoriesList> = ({ stories }) =
     <div className="container mx-auto mt-6 px-4">
       <DynamicTitleSection
         title={'Truyện mới cập nhật'}
-        href={'/more'}
+        href={'/list/truyen-moi'}
         linkText="Xem thêm"
       />
       <div className="grid grid-cols-1 gap-y-5 gap-x-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
